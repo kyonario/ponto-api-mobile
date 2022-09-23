eba2c7e93c2c633c635354a2552c07456b5b62a1
Kellisson Lima <kyonario@gmail.com>
2022-09-22 23:14:39 -0300

    fix-corrigindo testes pacotes

--- src/main/java/com/kynario/dev/PontoLegalApplication.java
+++ src/main/java/com/kynario/dev/PontoLegalApplication.java
@@ -2,6 +2,7 @@ package com.kynario.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -9,6 +10,11 @@ public class PontoLegalApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontoLegalApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PontoLegalApplication.class);
 	}
 
 }
