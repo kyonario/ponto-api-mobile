21748a4ebc4d306fb90cb6583bb0b902e8629f2a
Kellisson Lima <kyonario@gmail.com>
2022-09-22 21:36:44 -0300

    mudando o package no pom para poder gerar o war

--- src/main/java/com/kynario/dev/PontoLegalApplication.java
+++ src/main/java/com/kynario/dev/PontoLegalApplication.java
@@ -2,9 +2,10 @@ package com.kynario.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PontoLegalApplication {
+public class PontoLegalApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontoLegalApplication.class, args);
